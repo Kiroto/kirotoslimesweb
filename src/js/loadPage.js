61392c4657b4dd90ecdd9a23c2280107fea77c67
Jerry Rivas <kiroto50@gmail.com>
2021-08-26 09:55:14 -0400

    component loading fix

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -1,7 +1,7 @@
 $(() => {
-  $("#header").load(`${backpedalString}/src/components/header.html`);
-  $("#main-nav").load(`${backpedalString}/src/components/nav.html`);
-  $("#footer").load(`${backpedalString}/src/components/footer.html`);
+  $("#header").load(`/src/components/header.html`);
+  $("#main-nav").load(`/src/components/nav.html`);
+  $("#footer").load(`/src/components/footer.html`);
 });
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
