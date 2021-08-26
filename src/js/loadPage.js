5826d233a3a68b2cd0e539e191a909f58411b893
Jerry Rivas <kiroto50@gmail.com>
2021-08-26 11:04:20 -0400

    Custom font, better looking nav

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -1,7 +1,5 @@
 $(() => {
-  $("#header").load(`/src/components/header.html`);
-  $("#main-nav").load(`/src/components/nav.html`);
-  $("#footer").load(`/src/components/footer.html`);
+  $("#main-container").load(`/src/components/main.html`);
 });
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
